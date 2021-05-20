dde9a0fc447c76834d82903bad1045668726c213
vvvictorlee <lisheng.ipad@icloud.com>
2021-05-20 23:19:28 +0800

    add rpc

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # NFTBox
+
+
+
+curl -X POST \
+     -H 'Content-Type: application/json' \
+     -d '{"jsonrpc":"2.0","id":"id","method":"add","params":{"address":"0x4a79c58CCf9d80353c02357F26D6f7b99fA9991e"}}' \
+     http://localhost:6789/api/myboxes```
+```
--- server.js
+++ server.js
@@ -1,16 +1,30 @@
 const express = require('express')
-const serveStatic = require('serve-static')
 const path = require('path')
 const history = require('connect-history-api-fallback')
 
 const app = express()
 
-app.use(history({
-    // OPTIONAL: Includes more verbose logging
-    verbose: true
-}))
+// app.use(history({
+//     // OPTIONAL: Includes more verbose logging
+//     verbose: true
+// }))
+// app.use(express.bodyParser());
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }))
 
-app.use('/', serveStatic(path.join(__dirname, '/dist')))
-const port = process.env.PORT || 8089
+// parse requests of content-type - application/json
+app.use(express.json())
+
+// define a simple route
+// app.get('/', (req, res) => {
+//     res.json({"message": "."});
+// });
+
+require('./app/routes/nft.routes.js')(app);
+
+
+const port = process.env.PORT || 6789
 app.listen(port)
 console.log('Listening on port: ' + port)
+
+
