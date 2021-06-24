242bba2fd6fe2191b3431c1c06532d9aa43527b4
vvvictorlee <lisheng.ipad@icloud.com>
2021-06-24 22:18:20 +0800

    add tokenid url

--- app/routes/nft.routes.js
+++ app/routes/nft.routes.js
@@ -1,3 +1,6 @@
+const path = require('path')
+
+const fs = require('fs');
 module.exports = (app) => {
     const nft = require('../controllers/nft.controller.js');
 
@@ -5,7 +8,26 @@ module.exports = (app) => {
     app.post('/api/claimbadge', async function (req, res, next) {
         await nft.claimbadge(req, res);
     });
-   app.post('/api/mybadge', async function (req, res, next) {
+    app.post('/api/mybadge', async function (req, res, next) {
         await nft.mybadge(req, res);
     });
+    app.get('/badge/*', async function (req, res, next) {
+        // const qrcodeName = Date.now() + '-' + Math.random().toString(36).slice(-6);
+        const filePath = path.resolve(__dirname, `./gold_box.png`);
+        // 给客户端返回一个文件流
+        res.writeHead(200, { 'Content-Type': 'image/png' });
+        //格式必须为 binary，否则会出错
+        // 创建文件可读流
+        const cs = fs.createReadStream(filePath);
+        cs.on("data", chunk => {
+            res.write(chunk);
+        })
+        cs.on("end", () => {
+            res.status(200);
+            res.end();
+        })
+    });
+
+
+
 }
