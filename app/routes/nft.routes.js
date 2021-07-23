13eff44defaa66704e8690201a5b2e70c5aaeed7
vvvictorlee <lisheng.ipad@icloud.com>
2021-07-23 15:53:52 +0800

    add joint

--- app/routes/nft.routes.js
+++ app/routes/nft.routes.js
@@ -51,5 +51,17 @@ module.exports = (app) => {
             res.end();
         })
     });
+    app.get('/joint/*', async function (req, res, next) {
+        const filePath = path.resolve(__dirname, `./joint.png`);
+        res.writeHead(200, { 'Content-Type': 'image/png' });
+        const cs = fs.createReadStream(filePath);
+        cs.on("data", chunk => {
+            res.write(chunk);
+        })
+        cs.on("end", () => {
+            res.status(200);
+            res.end();
+        })
+    });
 
 }
