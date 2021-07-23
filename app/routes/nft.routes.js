eaedf16b3c75f99d598f2549a29ede3e4ac904fe
vvvictorlee <lisheng.ipad@icloud.com>
2021-07-23 15:51:39 +0800

    add png

--- app/routes/nft.routes.js
+++ app/routes/nft.routes.js
@@ -14,13 +14,9 @@ module.exports = (app) => {
     app.get('/api/ismaxtotalsupply', async function (req, res, next) {
         await nft.isMaxTotalSupply(req, res);
     });
-    app.get('/badge/*', async function (req, res, next) {
-        // const qrcodeName = Date.now() + '-' + Math.random().toString(36).slice(-6);
-        const filePath = path.resolve(__dirname, `./flag3_zh-hans.png`);
-        // 给客户端返回一个文件流
+    app.get('/joint/*', async function (req, res, next) {
+        const filePath = path.resolve(__dirname, `./joint.png`);
         res.writeHead(200, { 'Content-Type': 'image/png' });
-        //格式必须为 binary，否则会出错
-        // 创建文件可读流
         const cs = fs.createReadStream(filePath);
         cs.on("data", chunk => {
             res.write(chunk);
@@ -30,22 +26,7 @@ module.exports = (app) => {
             res.end();
         })
     });
-    app.get('/wofans/*', async function (req, res, next) {
-        // const qrcodeName = Date.now() + '-' + Math.random().toString(36).slice(-6);
-        const filePath = path.resolve(__dirname, `./wofans.png`);
-        // 给客户端返回一个文件流
-        res.writeHead(200, { 'Content-Type': 'image/png' });
-        //格式必须为 binary，否则会出错
-        // 创建文件可读流
-        const cs = fs.createReadStream(filePath);
-        cs.on("data", chunk => {
-            res.write(chunk);
-        })
-        cs.on("end", () => {
-            res.status(200);
-            res.end();
-        })
-    });
+    
 
 
 }
