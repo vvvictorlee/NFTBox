50872ff53041c15f525153ee90564117bd431cea
vvvictorlee <lisheng.ipad@icloud.com>
2021-07-31 11:11:25 +0800

    update

--- frontend/src/http/apiConfig.js
+++ frontend/src/http/apiConfig.js
@@ -3,5 +3,5 @@ export const ismaxtotalsupply = '/api/ismaxtotalsupply'; //查询是否领完
 export const claimbadge = 'api/claimbadge'; //领取徽章接口
 export const mybadge = 'api/mybadge'; //我的宝库
 
-export const gtapi = 'hoo';
+export const gtapi = 'hoo/api/third/v2/gtcaptcha';
 
